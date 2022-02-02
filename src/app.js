3a61eaa9f9f00e86cfeac8461b6420085699086b
Alex Wayland <89842550+web-lynx@users.noreply.github.com>
2022-02-02 00:47:38 +0000

    C, R and D done - just U to go

--- src/app.js
+++ src/app.js
@@ -1,16 +1,33 @@
 const yargs = require("yargs");
 const { client, connection } = require("./db/connection");
-const { addMovie } = require("./utils");
+const {
+  addMovie,
+  readAll,
+  readFilter,
+  deleteItem,
+  deleteAll,
+} = require("./utils");
 
 const app = async (yargsObj) => {
   try {
     const collection = await connection();
-    if (yargsObj.add) {
+    if (yargsObj.create) {
       await addMovie(collection, {
         title: yargsObj.title,
         actor: yargsObj.actor,
       });
-      console.log(`${yargsObj} added to the database.`);
+      console.log(`${yargsObj.title} added to the database.`);
+    } else if (yargsObj.readall) {
+      await readAll(collection);
+    } else if (yargsObj.readone) {
+      await readFilter(collection, {
+        title: yargsObj.title,
+        actor: yargsObj.actor,
+      });
+    } else if (yargsObj.deleteone) {
+      await deleteItem(collection, yargsObj);
+    } else if (yargsObj.deleteall) {
+      await deleteAll(collection, yargsObj);
     } else {
       console.log("Incorrect command.");
     }
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,104 @@
-exports.addMovie = async (collection, movieObj) => {
-  await collection.insertOne(movieObj);
+//Create
+exports.addMovie = async (collection, yargsObj) => {
+  await collection.insertOne(yargsObj);
 };
+
+//Read by key value
+exports.readFilter = async (collection, yargsObj) => {
+  if (yargsObj.title) {
+    query = yargsObj.title;
+    results = await collection.find({ title: query }).toArray();
+    console.log(`Found documents filtered by ${yargsObj.title} =>`, results);
+  } else if (yargsObj.actor) {
+    query = yargsObj.actor;
+    results = await collection.find({ actor: query }).toArray();
+    console.log(`Found documents filtered by ${yargsObj.actor} =>`, results);
+  }
+};
+
+//Read All
+exports.readAll = async (collection) => {
+  results = await collection.find({}).toArray();
+  console.log("Found all documents =>", results);
+};
+
+//Update
+exports.updateOne = async (collection, yargsObj) => {};
+
+//Delete One Instance
+exports.deleteItem = async (collection, yargsObj) => {
+  if (yargsObj.title) {
+    query = { title: [yargsObj.title] };
+    deleteResult = await collection.deleteOne(query);
+    if (deleteResult.deletedCount) {
+      console.log(`${deletedCount} items deleted.`);
+    } else {
+      console.log("No files deleted");
+    }
+  } else if (yargsObj.actor) {
+    query = { actor: yargsObj.actor };
+    console.log(query);
+    deleteResult = await collection.deleteOne(query);
+    if (deleteResult.deletedCount) {
+      console.log(`${deletedCount} items deleted.`);
+    } else {
+      console.log("No files deleted");
+    }
+  } else if (yargsObj.id) {
+    query = { id: yargsObj.id };
+    deleteResult = await collection.deleteOne(query);
+    if (deleteResult.deletedCount) {
+      console.log(`${deletedCount} items deleted.`);
+    } else {
+      console.log("No files deleted");
+    }
+  } else {
+    console.log("No files found with that value.");
+  }
+};
+
+//Delete All Instances
+exports.deleteAll = async (collection, yargsObj) => {
+  if (yargsObj.title) {
+    query = { title: [yargsObj.title] };
+    deleteResult = await collection.deleteMany(query);
+    if (deleteResult.deletedCount) {
+      console.log(`${deletedCount} items deleted.`);
+    } else {
+      console.log("No files deleted");
+    }
+  } else if (yargsObj.actor) {
+    query = { actor: [yargsObj.actor] };
+    deleteResult = await collection.deleteMany(query);
+    if (deleteResult.deletedCount) {
+      console.log(`${deletedCount} items deleted.`);
+    } else {
+      console.log("No files deleted");
+    }
+  } else if (yargsObj.id) {
+    query = { id: yargsObj.id };
+    deleteResult = await collection.deleteMany(query);
+    if (deleteResult.deletedCount) {
+      console.log(`${deletedCount} items deleted.`);
+    } else {
+      console.log("No files deleted");
+    }
+  } else {
+    console.log("No files found with that value.");
+  }
+};
+
+// switch (yargsObj) {
+//     case (yargsObj.title).length != 0:
+//
+//       }
+//       break;
+//     case yargsObj.actor:
+//       query = yargsObj.actor;
+//       deleteResult = await collection.deleteMany(query).toArray();
+//       console.log(deleteResult);
+//       break;
+//     default:
+//       console.log("Your switch doesn't work");
+//       break;
+//   }
